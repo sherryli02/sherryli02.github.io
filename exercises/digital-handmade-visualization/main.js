d5e779cba38eacbf245e1e92e024ba5c3477df42
Sherry <lisherry@seas.upenn.edu>
2022-04-11 19:34:39 -0400

    Update main.js

--- exercises/digital-handmade-visualization/main.js
+++ exercises/digital-handmade-visualization/main.js
@@ -4,9 +4,7 @@ let days = [
 	{name: "Sunday", speed: 80, amount: 80}
 ];
 
-// First show all the flowers and create divs for each of them
 let container = document.querySelector(".graph");
-// create a counter variable that will give us each flowers number in the array
 let i = 0;
 
 function display(day) {
@@ -19,13 +17,13 @@ function display(day) {
   newItem.classList.add("day");
   // adds a specific class to each new div
   newItem.classList.add("day" + [i]);
-  // places the flower name and amount to the div
+  // places the  name and amount to the div
   dayDiv.innerHTML = day.name;
-  // sets height of flower to the amount. try changing height to width, or padding
+  // sets height to the amount. try changing height to width, or padding
   dayDiv.style.letterSpacing = day.speed + "px";
   dayDiv.style.fontSize = day.amount + "px";
 
   console.log(day);
 }
-// loops through the flowers and runs the displayFlowers function for each one
+// loops through and runs the display function for each one
 days.forEach(display);
